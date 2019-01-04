af55d2c5c485378daec3937f03a065354d2bfa84
John F. Hogarty <john.hogarty@oracle.com>
2019-01-04 13:25:44 -0500

    WIP - working through the code....

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -14,7 +16,7 @@ import (
 
 var (
 	debug     = kingpin.Flag("debug", "Enable debug mode.").Bool()
-	timeout   = kingpin.Flag("timeout", "Timeout waiting for POST request.").Default("5s").Short('t').Duration()
+	timeout   = kingpin.Flag("timeout", "Timeout waiting for POST request.").Default("15s").Short('t').Duration()
 	files     = kingpin.Flag("files", "Path to file or directory of file(s) to parse and POST.").Short('f').Required().ExistingFileOrDir()
 	randomize = kingpin.Flag("randomize", "Enable randomization of data in JSON files.").Short('r').Bool()
 	endpoint  = kingpin.Flag("endpoint", "REST API endpoint to send request to.").Short('e').Required().String()
@@ -50,6 +52,26 @@ func main() {
 	// First, focus on doing a POST of each file, vanilla, to the endpoint.
 	// Once that is working properly, then look to add randomization and finally
 	// add concurrency.
+}
+
+func postJSON(jsonData []byte) {
+	fmt.Printf("DEBUG: url we will post JSON to: %v with a timeout of: %v \n", *endpoint, *timeout)
+
+	req, err := http.NewRequest("POST", *endpoint, bytes.NewBuffer(jsonData))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 }
 
 // At this time we will only support randomizing the value of top level keys.
@@ -88,7 +110,13 @@ func processFiles(f string) []string {
 			parsedFiles = append(parsedFiles, filename)
 		}
 	} else {
-		parsedFiles = append(parsedFiles, f)
+		jsonFile := isJSONFile(f)
+		if jsonFile == false || err != nil {
+			fmt.Printf("File %v is not a valid JSON file. \n", f)
+		} else {
+			parsedFiles = append(parsedFiles, f)
+		}
+
 	}
 	return parsedFiles
 }
@@ -135,6 +163,27 @@ func readJSONFile(file string) {
 	var result map[string]interface{}
 	json.Unmarshal([]byte(byteValue), &result)
 
+	// --------------------------------------------------------------
+	// TRIAL::
+	// --------------------------------------------------------------
+	req, err := http.NewRequest("POST", *endpoint, bytes.NewBuffer(byteValue))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth("perfapi", "f6cd3459f9a39c9784b3e328f05be0f7")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+	// --------------------------------------------------------------
+
 	// TRIAL - substitute a key/value **NOTE: THIS WORKS!!**
 	//result["hostname"] = "somerandom-host"
 
@@ -143,12 +192,12 @@ func readJSONFile(file string) {
 	//fmt.Println("Processed JSON mc_cfg: ", result["mc_cfg"])
 
 	// Pretty print the JSON
-	// if *debug {
-	// 	newResult, _ := json.MarshalIndent(result, "", "\t")
-	// 	if newResult != nil {
-	// 		fmt.Println(string(newResult))
-	// 	}
-	// }
+	if *debug {
+		newResult, _ := json.MarshalIndent(result, "", "\t")
+		if newResult != nil {
+			fmt.Println(string(newResult))
+		}
+	}
 }
 
 // TODO: need to build up/off of this.
